befcc04fad5cab73e99755463e9a5b9d2c92e2d4
Nicolas Erbetti <nerbetti@orange.fr>
2025-05-31 18:27:59 +0200

    Set default text color to white.

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" /> 
       </head>
       { /*Default style of the webpage's content.*/ }
-      <body className={`font-serifCustom antialiased`}>
+      <body className={`text-white font-serifCustom antialiased`}>
         {children}
       </body>
     </html>
